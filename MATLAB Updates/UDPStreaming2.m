c5bf22c6e4cb09fba330b56c0dc33966e17d221d
mem2agt <mem2agt@virginia.edu>
2024-06-02 15:17:09 -0400

    Update UDPStreaming2.m

--- MATLAB Updates/UDPStreaming2.m
+++ MATLAB Updates/UDPStreaming2.m
@@ -1,7 +1,5 @@
-%% Clearing everything to allow code to run
-close all;                     	% close all figures
-clear;                         	% clear all variables
-clc;                          	% clear the command terminal
+clear;
+clc;
 
 % Creating Path to needed class libraries
 % include x-IMU MATLAB library
@@ -11,125 +9,104 @@ addpath(['C:\Users\mccom\Documents\GitHub\VirtualOR2\MATLAB Updates\quaternion_l
 % Importing Mahony Function
 import MahonyAHRS.*
 
-%% Creating Connection To Arduino Uno
-% Create connection to Arduino Uno
+% Initialize Arduino and IMU
 UNO = arduino('COM3', 'Uno', 'Libraries', 'I2C');
-% Define Sample rate usually 100 hz or 200 hz
 imu = mpu6050(UNO, 'SampleRate', 200, 'SamplesPerRead', 1);
-
-%% Time Parameters, Sample Rate & Period
-% Define time (in seconds) visualization will run
-stopTimer = 5;
-% Get the current time
-ts = tic;
-
-SampleRate = 200;
-SamplePeriod = 1/200;
-
-%% Defining Filter, Thresholding, Initializing Arrays for orientation calc
-% Initialize Kalman filter
-fuse = imufilter('SampleRate',200);
+fuse = imufilter('SampleRate', 200);
 
 % Define threshold for zeroing gyroscope readings (in deg/s)
 gyroZeroThreshold = 0.5; % Adjust as needed
 
-% Initialize arrays to store raw data
-AccData = zeros(stopTimer*SampleRate, 3);
-GyroData = zeros(stopTimer*SampleRate, 3);
+% UDP setup
+receiverIP = '127.0.0.1';  % Unity host IP
+receiverPort = 8000;       % Unity host port
+udpClient = udp(receiverIP, receiverPort, 'LocalPort', 0);
+fopen(udpClient);
 
-%% Initializing Arrays for displacement calc
-% Calling AHRS function
-ahrs = MahonyAHRS('SamplePeriod', SamplePeriod, 'Kp', 1);
+disp('Connected to Unity host.');
 
-% Filter coefficients for high-pass filter
-cutoff_freq = (2 * 0.4) / (1 / SamplePeriod);
+% Initialize arrays for displacement calculation
+ahrs = MahonyAHRS('SamplePeriod', 1/200, 'Kp', 1);
+nyquistFreq = 200 / 2; % Nyquist frequency
+cutoff_freq = 0.4 / nyquistFreq; % Normalize cutoff frequency
 [b, a] = butter(1, cutoff_freq, 'high');
-
-% State variables for the filter (one for each dimension)
 z_velocity = zeros(max(length(a), length(b)) - 1, 3);
 z_position = zeros(max(length(a), length(b)) - 1, 3);
 
-% Initialize arrays to store calculated data
-tc_accel = zeros(stopTimer*SampleRate, 3);
-linear_accel = zeros(stopTimer*SampleRate, 3);
-linear_velocity = zeros(stopTimer*SampleRate, 3);
-cleaned_linear_velocity = zeros(stopTimer*SampleRate, 3);
-linear_position = zeros(stopTimer*SampleRate, 3);
-cleaned_linear_position = zeros(stopTimer*SampleRate, 3);
+linear_velocity = zeros(1, 3);
+linear_position = zeros(1, 3);
 
-% Initialize separate arrays to store displacement data for each axis
-DisplacementX = zeros(stopTimer*SampleRate, 1);
-DisplacementY = zeros(stopTimer*SampleRate, 1);
-DisplacementZ = zeros(stopTimer*SampleRate, 1);
+startTime = tic;
 
-%% Loop
-% Creating index for storing data 
-i = 1; 
+while true
+    try
+        data = table2array(imu.read());
+        accel = [-data(:, 2), -data(:, 1), data(:, 3)];
+        gyro = [data(:, 5), data(:, 4), -data(:, 6)];
 
-while (toc(ts) < stopTimer)
-    % Get the current elapsed time
-    elapsedTime = toc(ts);
-    disp(['Current time: ', num2str(elapsedTime), ' seconds']);
+        % Fuse accelerometer and gyroscope data to estimate device orientation
+        Viz_data = fuse(accel, gyro);
 
-    % Read raw data from IMU 
-    % Data will be provided as a time table therefore, must use the 
-    % table2array function 
-    data = table2array(imu.read());
+        % Calculate magnitude of angular velocity vector
+        gyroMagnitude = sqrt(sum(gyro.^2, 2));
 
-    % Assign the raw data measurements to accel and gyro 
-    accel = [-data(:,2), -data(:,1), data(:,3)];
-    gyro = [data(:,5), data(:,4), -data(:,6)];
+        % Zero gyroscope readings if angular velocity is below threshold
+        gyro(gyroMagnitude < gyroZeroThreshold, :) = 0;
 
-    % Fuse accelerometer and gyroscope data to estimate device orientation
-    Viz_data = fuse(accel, gyro);
+        % Implementing AHRS algorithms 
+        % Initializing orientation
+        ahrs.UpdateIMU(gyro * (pi / 180), accel);
+        R = quatern2rotMat(ahrs.Quaternion);
 
-    % Calculate magnitude of angular velocity vector
-    gyroMagnitude = sqrt(sum(gyro.^2, 2));
+        % Calculate 'tilt-compensated' accelerometer
+        tc_accel = R * accel';
 
-    % Zero gyroscope readings if angular velocity is below threshold
-    gyro(gyroMagnitude < gyroZeroThreshold, :) = 0;
+        % Calculate linear acceleration in Earth frame (subtracting gravity)
+        linear_accel = (tc_accel - [0; 0; 1])' * 9.81;
 
-    % Storing Data 
-    AccData(i,:) = accel;
-    GyroData(i,:) = gyro;
+        % Calculate linear velocity (integrate acceleration)
+        linear_velocity = linear_velocity + linear_accel * (1 / 200);
 
-    % Implementing AHRS algorithms 
-    % Initializing orientation
-    R = zeros(3,3);
-    ahrs.UpdateIMU(gyro*(pi/180),AccData(i,:));
-    R = quatern2rotMat(ahrs.Quaternion);
+        % Filtering linear velocity for each dimension
+        cleaned_linear_velocity = zeros(1, 3);
+        for dim = 1:3
+            [cleaned_linear_velocity(dim), z_velocity(:, dim)] = filter(b, a, linear_velocity(dim), z_velocity(:, dim));
+        end
 
-    % Calculate 'tilt-compensated' accelerometer
-    tc_accel(i,:) = R * AccData(i,:)';
+        % Calculate linear position (integrate velocity)
+        linear_position = linear_position + cleaned_linear_velocity * (1 / 200);
 
-    % Calculate linear acceleration in Earth frame (subtracting gravity)
-    linear_accel(i,:) = (tc_accel(i,:) - [0, 0, 1]) * 9.81;
+        % High-pass filter linear position to remove drift
+        cleaned_linear_position = zeros(1, 3);
+        for dim = 1:3
+            [cleaned_linear_position(dim), z_position(:, dim)] = filter(b, a, linear_position(dim), z_position(:, dim));
+        end
 
-    % Calculate linear velocity (integrate acceleration)
-    if i >= 2
-        linear_velocity(i,:) = linear_velocity(i-1,:) + linear_accel(i,:) * SamplePeriod;
+        % Displacement in meters
+        DisplacementX = cleaned_linear_position(1) / 175;
+        DisplacementY = cleaned_linear_position(2) / 175;
+        DisplacementZ = cleaned_linear_position(3) / 175;
+
+        % Get quaternion parts
+        [q0, q1, q2, q3] = parts(Viz_data);
+
+        % Print orientation and displacement data in one line
+        fprintf('Orientation: [%f, %f, %f, %f], Displacement: [%f, %f, %f]\n', ...
+            q0, q1, q2, q3, DisplacementX, DisplacementY, DisplacementZ);
+
+        % Send data to Unity via UDP
+        dataStr = sprintf('%.6f,%.6f,%.6f,%.6f', q0, q1, q2, q3);
+        fwrite(udpClient, dataStr);
+
+        pause(0.01);
+    catch ME
+        % If an error occurs, display the error message and attempt to reconnect
+        disp(['Error: ', ME.message]);
+        disp('Attempting to reconnect to Unity host...');
+        fclose(udpClient);
+        fopen(udpClient);
     end
-
-    % Filtering linear velocity for each dimension
-    for dim = 1:3
-        [cleaned_linear_velocity(i, dim), z_velocity(:, dim)] = filter(b, a, linear_velocity(i, dim), z_velocity(:, dim));
-    end
-
-    % Calculate linear position (integrate velocity)
-    if i >= 2
-        linear_position(i,:) = linear_position(i-1,:) + cleaned_linear_velocity(i,:) * SamplePeriod;
-    end
-
-    % High-pass filter linear position to remove drift
-    for dim = 1:3
-        [cleaned_linear_position(i, dim), z_position(:, dim)] = filter(b, a, linear_position(i, dim), z_position(:, dim));
-    end
-
-    % Store the displacement data in separate arrays
-    % Important to note that units of measure is: meters 
-    DisplacementX(i) = linear_position(i, 1)/175;
-    DisplacementY(i) = linear_position(i, 2)/175;
-    DisplacementZ(i) = linear_position(i, 3)/175;
-
-    i = i + 1; 
 end
+
+% Close UDP connection on exit
+fclose(udpClient);
